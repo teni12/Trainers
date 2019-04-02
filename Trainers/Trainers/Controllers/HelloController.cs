1b484461b2dfb99ac370d28cbcbe58138ce6f8e4
teni12 <48241562+teni12@users.noreply.github.com>
2019-04-02 12:25:10 +0100

    Add files via upload

---
+++ Trainers/Trainers/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloController : Controller
+    {
+        // 
+        // GET: /Hello/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /Hello/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
